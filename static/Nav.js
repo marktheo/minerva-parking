71e507655a97fe79d3ac13f1274769329ca1f519
Marktheo <marcosvinytheo@gmail.com>
2022-09-24 11:52:14 -0300

    WebProject Update

--- static/Nav.js
+++ static/Nav.js
@@ -8,7 +8,7 @@ setInterval(function() {
         nav_link[1].style.color = "black";
         nav_link[2].style.color = "black";
         nav_link[3].style.color = "black";
-        nav_img.src = "Media/Logo_Linear.png";
+        nav_img.src = "/static/Media/Logo_Linear.png";
         nav.style.backgroundColor = "#FFFFFF";
         nav.style.boxShadow = "1.5px 1.5px 6px rgb(0,0,0, 0.20)";
     } else {
@@ -16,7 +16,7 @@ setInterval(function() {
         nav_link[1].style.color = "white";
         nav_link[2].style.color = "white";
         nav_link[3].style.color = "white";
-        nav_img.src = "Media/Logo.png";
+        nav_img.src = "/static/Media/Logo.png";
         nav.style.backgroundColor = "";
         nav.style.boxShadow = "";
     }
