6815c40b058331861a48ad4d9c5538362b432091
Marktheo <marcosvinytheo@gmail.com>
2022-09-25 00:10:28 -0300

    WebProject Update

--- Render.py
+++ Render.py
@@ -2,8 +2,28 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def page():
-    return render_template("Product.html")
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/register")
+def register():
+    return render_template("Register.html")
+
+@app.route("/login")
+def login():
+    return render_template("Login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("Dashboard.html")
+
+@app.route("/schedule")
+def schedule():
+    return render_template("Schedule.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("Calendar.html")
 
 app.run()
