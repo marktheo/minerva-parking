994cb00c672e8dcf002da603cd120ae27ffbdaf0
Marktheo <marcosvinytheo@gmail.com>
2022-10-22 00:14:30 -0300

    WebProject Update

--- Render.py
+++ Render.py
@@ -1,43 +1,21 @@
-import sqlite3
+from flask import Flask, request, render_template
 
-sql = sqlite3.connect("database.db").cursor()
-
-from flask import Flask, request, render_template 
-
+#Flask Instance - Init
 app = Flask(__name__)
 
-@app.route("/about")
+#Main Page - About
+@app.route("/")
 def about():
     return render_template("About.html")
 
+#Secondary Page - Register
 @app.route("/register")
 def register():
     return render_template("Register.html")
 
+#Secondary Page - Login
 @app.route("/login")
 def login():
     return render_template("Login.html")
-
-@app.route("/userlogin", methods = ["POST"])
-def userlogin():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        return userlogin(username, password)
-    else:
-        return render_template("Login.html", errormessage = "Error 404")
-        
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("Dashboard.html")
-
-@app.route("/schedule")
-def schedule():
-    return render_template("Schedule.html")
-
-@app.route("/calendar")
-def calendar():
-    return render_template("Calendar.html")
 
 app.run()
